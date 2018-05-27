85158a8e8b9dd0aa833930b83d4e2257b191e664
Chris Broekema <broekema@astron.nl>
2018-05-27 23:26:35 +0200

    add initial BMP280 code skelleton

---
+++ src/SensorBMP280.cpp
@@ -0,0 +1,5 @@
+#include <SensorBMP280.h>
+
+SensorBMP280::SensorBMP280() {}
+void SensorBMP280::init() {}
+void SensorBMP280::measure() {}
---
+++ src/SensorBMP280.h
@@ -0,0 +1,20 @@
+
+#ifndef _SENSOR_BMP280_H_
+#define _SENSOR_BMP280_H_
+
+#include <Sensor.h>
+
+class SensorBMP280 : public 
+
+Sensor {
+
+    SensorBMP280();
+
+    void init();
+    void measure();
+
+
+
+} ;
+
+#endif
